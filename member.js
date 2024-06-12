7df020ba10e8d273a96807aa5ecda3020506f722
Jason M. Goebel <goebel.jason@gmail.com>
2024-06-12 13:36:22 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,29 @@
+function skillsMember() {
+  const member = this;
+  member.skills = [
+    {
+      name: 'HTML',
+      level: 5
+    },
+    {
+      name: 'CSS',
+      level: 5
+    },
+    {
+      name: 'JavaScript',
+      level: 4
+    },
+    {
+      name: 'React',
+      level: 4
+    },
+    {
+      name: 'Node.js',
+      level: 3
+    },
+    {
+      name: 'MongoDB',
+      level: 3
+    }
+  ];
+}
